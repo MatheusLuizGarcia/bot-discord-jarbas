614cc69b87dcfe1780be844095d71c4a2d77ca5f
Matheus Luiz Garcia <matheusluizgarcia99@gmail.com>
2025-03-27 19:48:48 -0300

    alterado opções de if elif pra match case

--- responses.py
+++ responses.py
@@ -2,11 +2,12 @@ from random import choice
 
 def get_response(user_input: str) -> str:
     lowered: str = user_input.lower()
-    if lowered == '':
-        return 'Você não disse nada...'
-    elif 'alo' in lowered or 'alô' in lowered:
-        return 'alo!!!'
-    elif 'coinflip' in lowered or 'cara ou coroa' in lowered:
-        return choice(['cara','coroa'])
-
-    # raise NotImplementedError('Chamada não implementada ainda!')
+    match lowered:
+        case '':
+            return 'Você não disse nada...'
+        case 'alo' | 'alô':
+            return 'alo!!!'
+        case 'coinflip' | 'cara ou coroa':
+            return choice(['cara','coroa'])
+        case _:
+            raise NotImplementedError('Chamada não implementada ainda!')
